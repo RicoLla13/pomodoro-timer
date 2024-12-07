fea2a59a677e7d89dc9e088c350c9f499ed54bbd
Liviu <liviuarsenescu@gmail.com>
2024-12-07 21:26:00 +0100

    done: counter gtk example

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,9 @@
-use gtk::glib::application_name;
+use std::cell::Cell;
+use std::rc::Rc;
+
+use glib::clone;
 use gtk::prelude::*;
-use gtk::{glib, Application, ApplicationWindow, Button};
+use gtk::{self, glib, Application, ApplicationWindow, Button, Label, Orientation};
 
 const APP_ID: &str = "some.ricolla1.PomodoroTimer";
 
@@ -13,22 +16,64 @@ fn main() -> glib::ExitCode {
 }
 
 fn build_ui(app: &Application) {
-    let button = Button::builder()
-        .label("Press me!")
+    let label = Label::builder()
         .margin_top(12)
         .margin_bottom(12)
         .margin_start(12)
         .margin_end(12)
         .build();
 
-    button.connect_clicked(|button| {
-        button.set_label("Hello World!");
-    });
+    let button_increase = Button::builder()
+        .label("+")
+        .margin_start(12)
+        .margin_end(12)
+        .build();
+
+    let button_decrease = Button::builder()
+        .label("-")
+        .margin_top(12)
+        .margin_bottom(12)
+        .margin_start(12)
+        .margin_end(12)
+        .build();
+
+    let number = Rc::new(Cell::new(0));
+    let label_text = format!("Current count: {}", number.get());
+    label.set_label(&label_text);
+
+    button_increase.connect_clicked(clone!(
+            #[weak]
+            number,
+            #[weak]
+            label,
+            move |_| {
+                number.set(number.get() + 1);
+                let label_text = format!("Current count: {}", number.get());
+                label.set_label(&label_text);
+            }
+    ));
+
+    button_decrease.connect_clicked(clone!(
+            #[weak]
+            label,
+            move |_| {
+                number.set(number.get() - 1);
+                let label_text = format!("Current count: {}", number.get());
+                label.set_label(&label_text);
+            }
+    ));
+
+    let gtk_box = gtk::Box::builder()
+        .orientation(Orientation::Vertical)
+        .build();
+    gtk_box.append(&label);
+    gtk_box.append(&button_increase);
+    gtk_box.append(&button_decrease);
 
     let window = ApplicationWindow::builder()
         .application(app)
         .title("Pomodoro Timer")
-        .child(&button)
+        .child(&gtk_box)
         .build();
 
     window.present();
